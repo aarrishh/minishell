34c5dca6b9b14165be6e39d4e79302a24bc5f7e7
Arina Manukyan <arimanuk@c2r1s5.42yerevan.am>
2025-08-13 17:08:59 +0400

    added global variable for 0 and added function to handle it

--- built-in/exit.c
+++ built-in/exit.c
@@ -6,7 +6,7 @@
 /*   By: arimanuk <arimanuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 15:23:54 by arina             #+#    #+#             */
-/*   Updated: 2025/08/11 18:03:03 by arimanuk         ###   ########.fr       */
+/*   Updated: 2025/08/13 16:59:35 by arimanuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,12 +43,16 @@ int	exit_command(t_token **stack, t_env **env, char **split)
 		{
 			printf("exit\nminishell: exit: %s: numeric argument required\n", \
 			tmp->string);
+			g_exit_status = 2;
 			// free_all(env, stack, split);
 			exit(2);
 		}
 	}
 	if (exit_command_print_function(tmp, num) == 1)
+	{
+		g_exit_status = 1;
 		return (1);
+	}
 	return (0);
 }
 
