e019acd1cfa716d3392bfc83d6ff8f923287fdd1
Arina <a.manukyan2006@gmail.com>
2025-08-29 19:20:15 +0400

    some changes about stderr and stdout, and please check Makefile, I leave you message

--- built-in/exit.c
+++ built-in/exit.c
@@ -6,7 +6,7 @@
 /*   By: arina <arina@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 15:23:54 by arina             #+#    #+#             */
-/*   Updated: 2025/08/25 13:16:49 by arina            ###   ########.fr       */
+/*   Updated: 2025/08/29 19:09:17 by arina            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	exit_command_print_function(t_token *tmp, long long num)
 	if (tmp->next)
 	{
 		g_exit_status = 127;
-		return (printf("exit\nminishell: exit: too many arguments\n"), 1);
+		return (ft_putendl_fd("exit\nminishell: exit: too many arguments", 2), 1);
 	}
 	else
 	{
@@ -46,8 +46,9 @@ int	exit_command(t_token **stack, t_env **env, char **split)
 		flag = ft_atol(tmp->string, &num);
 		if (flag == -1)
 		{
-			printf("exit\nminishell: exit: %s: numeric argument required\n", \
-			tmp->string);
+			write (2, "exit\nminishell: exit: ", 23);
+			write (2, &tmp->string, ft_strlen(tmp->string));
+			write (2, ": numeric argument required\n", 29);
 			g_exit_status = 2;
 			free_all(env, stack, &split);
 			rl_clear_history();
