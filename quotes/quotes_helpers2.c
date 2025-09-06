acac424c36ed8344ad5a4d6108849a9417a83f57
Manana Baghdasaryan <mabaghda@c2r2s1.42yerevan.am>
2025-09-06 13:06:12 +0400

    fixed expanding

--- quotes/quotes_helpers2.c
+++ quotes/quotes_helpers2.c
@@ -6,7 +6,7 @@
 /*   By: mabaghda <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 14:16:09 by mabaghda          #+#    #+#             */
-/*   Updated: 2025/09/06 12:54:25 by mabaghda         ###   ########.fr       */
+/*   Updated: 2025/09/06 13:05:42 by mabaghda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,6 +88,12 @@ void	exp_help_loop(t_quote_state state, char *str, char *new, t_iter *ij,
 		if (str[ij->i] == '"')
 			(ij->i)++;
 	}
+}
+
+int	check_valid_dollar(char chr)
+{
+	return (chr == '_' || (chr >= 'A' && chr <= 'Z') || (chr >= 'a'
+			&& chr <= 'z') || (chr >= '0' && chr <= '9'));
 }
 
 char	*find_var_value(char *str, t_env **env, int *key_length)
